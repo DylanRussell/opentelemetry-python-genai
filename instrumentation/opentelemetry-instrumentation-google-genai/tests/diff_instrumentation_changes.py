84db02023a4af3e07668767edd3674acbd96df0c
Dylan Russell <dylanrus@gmail.com>
2026-05-15 20:25:32 +0000

    Run precommit

--- instrumentation/opentelemetry-instrumentation-google-genai/tests/diff_instrumentation_changes.py
+++ instrumentation/opentelemetry-instrumentation-google-genai/tests/diff_instrumentation_changes.py
@@ -17,32 +17,35 @@ import os
 
 from google import genai
 from google.genai import types
+from google.protobuf import text_format
+
+from opentelemetry import _logs as logs
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.common._internal import (
+    _log_encoder,
+    trace_encoder,
+)
 from opentelemetry.instrumentation.google_genai import (
     GoogleGenAiSdkInstrumentor,
 )
-import time
 from opentelemetry.sdk._logs import LoggerProvider
-from opentelemetry import trace
-from opentelemetry.sdk._logs.export import (
-    SimpleLogRecordProcessor,
-)
-
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import SimpleSpanProcessor
-from opentelemetry.exporter.otlp.proto.common._internal import trace_encoder, _log_encoder
-from google.protobuf import text_format
 from opentelemetry.sdk._logs._internal.export.in_memory_log_exporter import (
     InMemoryLogExporter,
 )
-from opentelemetry import _logs as logs
+from opentelemetry.sdk._logs.export import (
+    SimpleLogRecordProcessor,
+)
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 from opentelemetry.sdk.trace.export.in_memory_span_exporter import (
     InMemorySpanExporter,
 )
 
-
 os.environ["OTEL_SEMCONV_STABILITY_OPT_IN"] = "gen_ai_latest_experimental"
-os.environ["OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT"] = "SPAN_AND_EVENT""
+os.environ["OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT"] = (
+    "SPAN_AND_EVENT"
+)
 
 PROJECT_ID = "my-project-id"
 LOCATION = "us-central1"
@@ -54,9 +57,7 @@ in_memory_log_exporter = InMemoryLogExporter()
 in_memory_span_exporter = InMemorySpanExporter()
 
 trace_provider = TracerProvider(resource=resource)
-trace_provider.add_span_processor(
-    SimpleSpanProcessor(in_memory_span_exporter)
-)
+trace_provider.add_span_processor(SimpleSpanProcessor(in_memory_span_exporter))
 trace.set_tracer_provider(trace_provider)
 logger_provider = LoggerProvider(resource=resource)
 logger_provider.add_log_record_processor(
@@ -67,25 +68,31 @@ logger_provider.add_log_record_processor(
 )
 logs.set_logger_provider(logger_provider)
 
+
 def write_logs_to_file(file_name: str):
     logs = in_memory_log_exporter.get_finished_logs()
     log_proto = _log_encoder.encode_logs(logs)
     with open(os.path.join(os.getcwd(), file_name + ".textproto"), "w") as f:
         f.write(text_format.MessageToString(log_proto))
 
-def write_spans_to_file(file_name: str):
-  spans = in_memory_span_exporter.get_finished_spans()
-  span_proto = trace_encoder.encode_spans(spans)
-  with open(os.path.join(os.getcwd(), file_name + ".textproto"), "w") as f:
-    f.write(text_format.MessageToString(span_proto))    
 
-def add(a: int, b:int) -> int:
+def write_spans_to_file(file_name: str):
+    spans = in_memory_span_exporter.get_finished_spans()
+    span_proto = trace_encoder.encode_spans(spans)
+    with open(os.path.join(os.getcwd(), file_name + ".textproto"), "w") as f:
+        f.write(text_format.MessageToString(span_proto))
+
+
+def add(a: int, b: int) -> int:
     return a + b
+
 
 def main():
     GoogleGenAiSdkInstrumentor().instrument()
     client = genai.Client(
-        vertexai=True, project=PROJECT_ID, location=LOCATION,
+        vertexai=True,
+        project=PROJECT_ID,
+        location=LOCATION,
     )
     response = client.models.generate_content(
         model=MODEL,
